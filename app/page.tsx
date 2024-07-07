9957fee09dba15fa47a2e430f4a0390a2f815403
Aditya Jaka Prasaja <aditya.jakap12@gmail.com>
2024-07-07 23:45:37 +0700

    commit baru

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import styles from '@/app/ui/home.module.css';
 import { lusitana } from './ui/fonts';
 import Image from 'next/image';
 
-
+//command baru
 export default function Page() {
   return (
     <main className="flex min-h-screen flex-col p-6">
